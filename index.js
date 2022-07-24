5879d1286610ee5e162d5f2602fc2e64a7a36e1e
ytasan <info@yasintasan.com>
2022-07-25 02:20:53 +0300

    todo listesi

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // todo = yazdirilan zaman degerinin soldan saga kaymasi - sebebi: milisaniye degerinin basamak sayisinin degismesi 
 
+// todo: pop up "are you sure?" for reset button 
+// document.querySelector('#splitbutton').addEventListener('click', function() {
+//     console.log("split button pressed");
+// });
+
+// todo: pause durumunda saymanin durdurulmasi 
+// todo: pause log'unun yazdirilmasi - pause tusu hem split hem pause anlamina geliyor. 
+// todo: split log'unun yazdirilmasi
+
 let startButtonText = document.querySelector("#startButton");
 let splitButton = document.querySelector("#splitButton");
 let resetButton = document.querySelector("#resetButton");
@@ -30,18 +39,14 @@ document.querySelector('#startButton').addEventListener('click', function() {
 
     setInterval( () => {
         let passedTime = Date.now() - startTime;
-        let passedTimeMiliSecond = passedTime%1000;
-        let passedTimeSecond = (Math.floor((passedTime/1000)%1000)%60);
-        let passedTimeMinute = Math.floor((((passedTime/60)/1000)%1000)%60);
-        let passedTimeHour = Math.floor(((((passedTime/60)/60)/1000)%1000)%60);
+        let passedTimeMiliSecond = passedTime%999;
+        let passedTimeSecond = Math.floor((passedTime/999)%1000)%60;
+        let passedTimeMinute = Math.floor((((passedTime/59)/1000)%1000)%60);
+        let passedTimeHour = Math.floor(((((passedTime/59)/60)/1000)%1000)%60);
 
         document.querySelector('.stopWatchDisplay').textContent = `${passedTimeHour}:${passedTimeMinute}:${passedTimeSecond}.${passedTimeMiliSecond}`;        
     },1);
 
-    // todo: stop counting
+
 });
  
-// todo: pop up "are you sure?" for reset button 
-// document.querySelector('#splitbutton').addEventListener('click', function() {
-//     console.log("split button pressed");
-// });
