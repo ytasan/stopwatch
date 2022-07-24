d638632b33eb72f27a04fd3290c33db2926e5a31
ytasan <info@yasintasan.com>
2022-07-25 01:54:58 +0300

    butonlarin enable/disable edilmesi tamam

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 // todo = yazdirilan zaman degerinin soldan saga kaymasi - sebebi: milisaniye degerinin basamak sayisinin degismesi 
 
+let startButtonText = document.querySelector("#startButton");
+let splitButton = document.querySelector("#splitButton");
+let resetButton = document.querySelector("#resetButton");
+splitButton.disabled = true;
+resetButton.disabled = true;
 
+let startButtonClickCounter = 0;
 
 document.querySelector('#startButton').addEventListener('click', function() { 
 
-    let startButtonText = document.querySelector("#startButton");
+    startButtonClickCounter++;
 
     if (startButtonText.innerHTML === "Start") {
         startButtonText.innerHTML = "Pause";
+        splitButton.disabled = false;
     } else {
         startButtonText.innerHTML = "Start";
+        splitButton.disabled = true;
+    }
+
+    if ((startButtonClickCounter >= 1) && (startButtonText.innerHTML === "Start")) {
+        resetButton.disabled = false;
+    } else {
+        resetButton.disabled = true;
     }
 
     let startTime = Date.now(); 
@@ -24,9 +38,10 @@ document.querySelector('#startButton').addEventListener('click', function() {
         document.querySelector('.stopWatchDisplay').textContent = `${passedTimeHour}:${passedTimeMinute}:${passedTimeSecond}.${passedTimeMiliSecond}`;        
     },1);
 
-
-
-
+    // todo: stop counting
 });
-
-
+ 
+// todo: pop up "are you sure?" for reset button 
+// document.querySelector('#splitbutton').addEventListener('click', function() {
+//     console.log("split button pressed");
+// });
